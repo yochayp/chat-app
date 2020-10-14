b960266e3d41948863b430e86ad82a24829b8828
yochayp <53395640+yochayp@users.noreply.github.com>
2020-10-14 17:56:54 +0300

    fix routes

--- routes.js
+++ routes.js
@@ -1,29 +1,33 @@
 const routeHandler = require('./routeHandler');
 const multer = require('multer');
 
-class Routes{
-    
-    constructor(app){
+class Routes {
+
+    constructor(app) {
         this.app = app;
     }
 
-    initRoutes(){
+    initRoutes() {
         // SET STORAGE
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
-  })
-  
-  var upload = multer({ storage: storage })
-        
+        var storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, 'uploads')
+            },
+            filename: function (req, file, cb) {
+                cb(null, file.fieldname + '-' + Date.now())
+            }
+        })
+
+        var upload = multer({ storage: storage })
+
         this.app.post('/handleSignup', upload.single('avatar'), routeHandler.handleSignUp)
-       
+
         this.app.get('/handleLogin', routeHandler.handleLogIn)
-       
+
+        this.app.get("*", (req, res) => {
+            res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+        });
+
         this.app.get("/", (req, res) => res.send('hello world'))
 
     }
